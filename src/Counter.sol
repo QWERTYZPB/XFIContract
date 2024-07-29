910afd3a166b2539f24c3981361da8df52ba532b
QWERTYZPB <qwertyzpb@gmail.com>
2024-07-29 03:56:31 +0300

    Changes to be committed: 	modified:   src/Counter.sol

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,21 +1,39 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
-// Deployer: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
-// Deployed to: 0x5FbDB2315678afecb367f032d93F642f64180aa3
-// Transaction hash: 0xdf126a66fec968bf9311660ad40f7ea662f6c4adab41bbdd9b158a43ff14f205
-
-
-contract Faucet {
-    // Accept any incoming amount
-    receive() external payable {}
-
-    // Give out ether to anyone who asks
-    function withdraw(uint withdraw_amount) public {
-        // Limit withdrawal amount
-        require(withdraw_amount <= 0.01 ether);
-        return withdraw_amount;
-        // Send the amount to the address that requested it
-        payable(msg.sender).transfer(withdraw_amount);
+contract Counter {
+    address owner;
+    
+    constructor() {
+        owner = msg.sender;    
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the contract owner");
+        _;
+    }
+    
+    function getBalanceOfContract() public view returns(uint) {
+        return address(this).balance;
+    }
+    
+    function getBalanceOfOwner() public view onlyOwner returns(uint){
+        return owner.balance;
+    }
+    
+    function getBalanceOfSender() public view returns(uint) {
+        return msg.sender.balance;
+    }
+    
+    function getAddressOfContract() public view returns(address) {
+        return address(this);
+    }
+   
+    function getAddressOfOwner() public view returns(address) {
+        return owner;    
+    }
+     
+    function getAddressOfSender() public view returns(address) {
+        return msg.sender;
     }
 }
